f097f8c980ea94fbb6925bfaf373861d0807d212
Igor Tronin <igortronin2705@gmail.com>
2022-02-16 16:15:25 +0400

    React_9_Char

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -11,7 +11,6 @@ class MarvelService {
         if(!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
         return await res.json();
     }
 
@@ -19,6 +18,7 @@ class MarvelService {
         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
+    
 
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
@@ -28,11 +28,13 @@ class MarvelService {
     _transformCharacter = (char) => {
 
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 120)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
